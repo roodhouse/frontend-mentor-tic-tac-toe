73ff853a305389fb824ab976a93a2f6f66837ea4
roodhouse <rughjm@gmail.com>
2023-08-13 19:17:04 -0500

    init setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      lightBlue: '#31C3BD',
+      lightBlueHover: '#65E9E4',
+      lightYellow: '#F2B137',
+      lightYellowHover: '#FFC860',
+      darkNavy: '#1A2A33',
+      semiDarkNavy: '#1F3641',
+      silver: '#A8BFC9',
+      silverHover: '#DBE8ED'
+    }
+  },
+  plugins: [],
+}
