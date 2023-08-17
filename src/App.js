7b1fde460ca77856c80752b2f7959bf6b9e03cf5
roodhouse <rughjm@gmail.com>
2023-08-17 12:25:12 -0500

    some winning logic added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Modules from './components/Modules';
     // game grid logic
       // make function to increment
         // update function logic to only increment after a win or tie is determine
+          // create a way to win array
+            // then if either the x or o array include the ways to win then its a win
   // module logic
 
 // game multiplayer designs
@@ -43,19 +45,49 @@ function App() {
     setTurn(newPlayer)
   }
 
-  // Increment scores
-  // increment x score
-  useEffect(() => {
-    function roundOver() {
-      if (xArray.length > 0) {
-        setXScore(xScore + 1)
-      } 
+  let ways2win = 
+    {
+      row1: 'square0,square1,square2',
+      row2: 'square3,square4,square5',
+      row3: 'square6,square7,square8',
+      col1: 'square0,square3,square6',
+      col2: 'square1,square4,square7',
+      col3: 'square2,square5,square8',
+      dia1: 'square0,square4,square8',
+      dia2: 'square2,square4,square6'
     }
+
+  let xArrayString = xArray.toString()
+  // Convert xArrayString to an array of individual items
+  let xArrayItems = xArrayString.split(',');
+
+  // Check if any of the winning combinations are present in xArrayItems
+
+  // Object.values(ways2win): 
+    // This retrieves an array containing the values (combinations of squares) from the ways2win object. 
+    // Each value is a string representing a winning combination of square IDs.
+  // .some(combination => ... ): 
+    // The .some() method is used on the array of combinations. 
+    //  It checks if at least one combination (string of square IDs) satisfies the condition defined inside the arrow function.
+  // combination.split(','): 
+    // This splits the current combination string into an array of individual square IDs. 
+    // The split(',') function is used to separate the IDs using a comma as the delimiter.
+  // .every(item => xArrayItems.includes(item)): 
+    // The .every() method is used on the array of split square IDs. 
+    // It checks if all elements of the array satisfy the condition defined inside the arrow function.
+  // xArrayItems.includes(item): 
+    // This checks if the current square ID (item) exists in the xArrayItems array, 
+    // which contains the individual square IDs from the xArray.
+
+  let isWinner = Object.values(ways2win).some(combination =>
+  combination.split(',').every(item => xArrayItems.includes(item))
+);
+
+if (isWinner) {
+  console.log('winner');
+}
+
   
-    roundOver()
-
-  },[xArray.length])
-
   // increment o score
   useEffect(() => {
     function roundOver() {
@@ -69,7 +101,7 @@ function App() {
   // increment tie count
   useEffect(() => {
     function roundOver() {
-      if (xArray.length === 1 && oArray.length === 1) {
+      if (xArray.length === 5 && oArray.length === 4) {
         setTies(ties + 1)
       }
     }
@@ -85,6 +117,9 @@ function App() {
   }
 
   console.log(`Player x: ${xArray}`)
+
+  console.log(typeof xArray)
+  console.log(xArrayString)
 
   console.log(`Player o: ${oArray}`)
 
