eedeb4f3db389e2118cb48c82fb4c324c13f56e2
roodhouse <rughjm@gmail.com>
2023-08-19 16:29:59 -0500

    user O vs CPU X done

--- src/components/GameOn.jsx
+++ src/components/GameOn.jsx
@@ -3,7 +3,7 @@ import GameOnHeader from './gameOn/GameOnHeader'
 import GameGrid from './gameOn/GameGrid'
 import ScoreCard from './gameOn/ScoreCard'
 
-function GameOn({turn, changeTurn, theCompetition, choice, xScore, oScore, ties, xArray, oArray, playerXarray, playerOarray, restarted, newRound }) {
+function GameOn({turn, changeTurn, theCompetition, choice, xScore, oScore, ties, xArray, oArray, playerXarray, playerOarray, restarted, newRound, winner }) {
   return (
     <>
         <div id="gameOnContainer">
@@ -11,7 +11,7 @@ function GameOn({turn, changeTurn, theCompetition, choice, xScore, oScore, ties,
                 <GameOnHeader turn={turn} />
             </div>
             <div id="gameGridWrapper">
-                <GameGrid turn={turn} changeTurn={changeTurn} xArray={xArray} oArray={oArray} playerXarray={playerXarray} playerOarray={playerOarray} restarted={restarted} newRound={newRound} theCompetition={theCompetition} choice={choice}  />
+                <GameGrid turn={turn} changeTurn={changeTurn} xArray={xArray} oArray={oArray} playerXarray={playerXarray} playerOarray={playerOarray} restarted={restarted} newRound={newRound} theCompetition={theCompetition} choice={choice} winner={winner} />
             </div>
             <div id="scoreCardWrapper">
                 <ScoreCard turn={turn} theCompetition={theCompetition} choice={choice} xScore={xScore} oScore={oScore} ties={ties} />
